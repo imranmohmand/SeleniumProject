74df11d941ed18fe594e6b25b55572fa3eead593
imran <imranmohmand@yahoo.com>
2020-08-07 23:52:46 -0400

    just random class created to check jenkin run automatically after one minute

---
+++ src/kamalaInstructor/TestingJenkinGithup.java
@@ -0,0 +1,12 @@
+package kamalaInstructor;
+
+import org.testng.annotations.Test;
+
+public class TestingJenkinGithup {
+	
+	@Test
+	public void Hello() {
+		System.out.println(" this is hello method");
+	}
+
+}
